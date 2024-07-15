37146722c38472854ea3ae3e240bc033481868ed
Taylor Code <taylorcode.projects@gmail.com>
2024-07-14 18:56:38 -0600

    feat(exercise 1): copy solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,16 +29,32 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
         <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <SubLink href="/">Already a subscriber?</SubLink>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
 };
 
 const SuperHeader = styled.div`
-  padding: 16px 0;
   background: var(--color-gray-900);
+  padding: 16px 0;
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +75,53 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+    justify-self: end;
+    position: relative;
+  }
+`;
+
+const SubLink = styled.a`
+  color: var(--color-gray-900);
+  font-size: 0.875rem;
+  font-style: italic;
+  margin-block-start: 8px;
+  position: absolute;
+  text-align: center;
+  text-decoration: underline;
+  width: 100%;
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
   align-items: center;
+  display: flex;
   justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+  margin-block: 32px 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-block: 48px 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    align-items: center;
+    display: grid;
+    grid-template-columns: 1fr auto 1fr;
+    justify-content: revert;
+    justify-items: start;
+    margin-block: 16px 72px;
+  }
 `;
 
 export default Header;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,15 +2,15 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import format from 'date-fns/format';
 
+import { QUERIES } from '../../constants';
+
 const Logo = (props) => {
   return (
     <Wrapper>
       <Link href="/" {...props}>
         New Grid Times
       </Link>
-      <TodaysDate>
-        {format(new Date(), 'EEEE, MMMM do, yyyy')}
-      </TodaysDate>
+      <TodaysDate>{format(new Date(), 'EEEE, MMMM do, yyyy')}</TodaysDate>
     </Wrapper>
   );
 };
@@ -22,12 +22,16 @@ const Wrapper = styled.div`
 const Link = styled.a`
   font-family: var(--font-family-logo);
   font-size: 3rem;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: 4rem;
+  }
 `;
 
 const TodaysDate = styled.p`
   font-size: 1.125rem;
   font-weight: var(--font-weight-medium);
-  margin-top: -1rem;
+  margin-block-start: -1rem;
 `;
 
 export default Logo;
